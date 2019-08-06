32547f4d7d4f04a2d930f958ba48ef9189903e24
saraswathia <sasuthkar56@gmail.com>
2019-08-06 19:15:07 +0530

    Done1

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -4,8 +4,8 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
-		return c;
+		String a = "Hello World!";
+		return a;
 	}
 	public static int add(int a,int b) {
 		int c = a+b;
@@ -13,6 +13,6 @@ public class SampleProject {
 	}
 	public static void main(String args[]) {
 		System.out.println(printHello());
-		System.out.println(add(23,45));
+		System.out.println(add(235,455));
 	}
 }
